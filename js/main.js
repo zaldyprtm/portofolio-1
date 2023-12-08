d0288064c79c9b702787b47c5fbb6f46a73df2f1
zaldyprtm <zaldyprtm@gmail.com>
2023-12-08 21:27:30 +0700

    js fix

--- js/main.js
+++ js/main.js
@@ -1,40 +1,47 @@
-const searchButton = document.querySelector('nav .desktop-nav .link-search');
-const closeButton = document.querySelector('.search-container .link-close');
-const desktopNav = document.querySelector('.desktop-nav');
-const searchContainer = document.querySelector('.search-container');
-const overlay = document.querySelector('.overlay');
+var searchButton = document.querySelector('nav .desktop-nav .link-search');
+var closeButton = document.querySelector('.search-container .link-close');
+var desktopNav = document.querySelector('.desktop-nav');
+var searchContainer = document.querySelector('.search-container');
+var overlay = document.querySelector('.overlay');
 
-searchButton.addEventListener('click', () => {
+
+if(searchButton){
+searchButton?.addEventListener('click', () => {
     desktopNav.classList.add('hide');
     searchContainer.classList.remove('hide');
     overlay.classList.add('show');
-})
+})}
 
+if(closeButton){
 closeButton.addEventListener('click', () => {
     desktopNav.classList.remove('hide');
     searchContainer.classList.add('hide');
     overlay.classList.remove('show');
 })
-
+}
+if(overlay){
 overlay.addEventListener('click', () => {
     desktopNav.classList.remove('hide');
     searchContainer.classList.add('hide');
     overlay.classList.remove('show');
 })
-
+}
 // mobile version
 
-const menuIconContainer = document.querySelector('nav .menu-icon-container');
-const navContainer = document.querySelector('.nav-container');
+var menuIconContainer = document.querySelector('nav .menu-icon-container');
+var navContainer = document.querySelector('.nav-container');
 
+if(menuIconContainer){
 menuIconContainer.addEventListener('click', () => {
     navContainer.classList.toggle('active');
 })
+}
+var searchbar = document.querySelector('.mobile-search-container .search-bar');
+var nav = document.querySelector('.nav-container nav');
+var searchInput = document.querySelector('.mobile-search-container input');
+var cancelBtn = document.querySelector('.mobile-search-container .cancel-btn');
 
-const searchbar = document.querySelector('.mobile-search-container .search-bar');
-const nav = document.querySelector('.nav-container nav');
-const searchInput = document.querySelector('.mobile-search-container input');
-const cancelBtn = document.querySelector('.mobile-search-container .cancel-btn');
+if(searchInput){
 
 searchInput.addEventListener('click', () => {
     searchbar.classList.add('active');
@@ -42,10 +49,12 @@ searchInput.addEventListener('click', () => {
     desktopNav.classList.add('move-down');
 
 })
-
+}
+if(cancelBtn){
 cancelBtn.addEventListener('click', () => {
     searchbar.classList.remove('active');
     nav.classList.remove('move-up');
     desktopNav.classList.remove('move-down');
 })
+}
 
